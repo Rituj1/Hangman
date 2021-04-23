90c8eb561235e70427d27e4b2d48bfa10edc9575
Rituj Srivastava <rituj1103@gmail.com>
2021-04-23 18:11:01 +0530

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -1,7 +1,9 @@
 import random
+from hang import logo
+print(logo)
+
 print('Welcome to the Hangman Game!')
 print("You have six lives to guess and win the game and if you aren't able to do so you will be hanged to death!")
-
 
 
 word = ['apple', 'banana', 'cherry', 'dragonfruit', 'strawberry']
@@ -19,70 +21,12 @@ elif chosen_word == 'strawberry':
     print("Hint: You can eat it but you can't straw it!")
 
 
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
-
-
 
 display = []
 for _ in range(len(chosen_word)):
     display += '_'
 print(display)
-lives = 6
+lives = 6  #Number of Lives
 game_ends = False
 while not game_ends:
     user_entry = input('Guess a letter: ').lower()
@@ -91,18 +35,18 @@ while not game_ends:
         if letter == user_entry:
             display[position] = letter
 
-    print(f"{' '.join(display)}")
+    print(f"{' '.join(display)}") #Used to convert the list into string.
 
 
     if user_entry not in chosen_word:
-        lives -= 1
+        lives -= 1    # Used to count the number of lives user has.
         if lives == 0:
-            game_ends = True
+            game_ends = True  #Ends the game.
             print('You Lost!\nGame Over!')
 
     if '_' not in display:
-        game_ends = True
+        game_ends = True   #Ends the game.
         print('You Win!')
-
+    from hang import stages
     print(stages[lives])
 
